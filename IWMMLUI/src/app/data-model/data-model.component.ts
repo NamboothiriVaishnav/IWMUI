e43f48865886a5fc822b598089a1e473a885909c
sivakumar <sivakumar.kokku@neudesic.com>
2022-11-23 18:44:15 +0530

    commiting data model changes

--- IWMMLUI/src/app/data-model/data-model.component.ts
+++ IWMMLUI/src/app/data-model/data-model.component.ts
@@ -1,4 +1,10 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { DataModel, IDataModel } from '../Models/dataModel';
+
+
+
 
 @Component({
   selector: 'app-data-model',
@@ -6,10 +12,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./data-model.component.scss']
 })
 export class DataModelComponent implements OnInit {
+  dataModelDetails: IDataModel[] = DataModel;
+  dataModelInnerHtmlPage: any;
 
-  constructor() { }
+  constructor(private sanitizer: DomSanitizer, private http: HttpClient) { }
 
   ngOnInit(): void {
+  }
+  showModel(modelId: any) {
+    let dataModelHtmlFileDetail = this.dataModelDetails.filter(x => x.id === modelId)[0].htmlFilePath;
+    this.http.get(dataModelHtmlFileDetail, { responseType: 'text' })
+      .subscribe(logo => {
+        this.dataModelInnerHtmlPage = this.sanitizer.bypassSecurityTrustHtml(logo);
+      });
   }
 
 }
